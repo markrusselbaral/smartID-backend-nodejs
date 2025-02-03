cc3520fc0c63df788dafb2cf18abda7abc420905
markrusselbaral <nullcracker04@gmail.com>
2025-02-03 21:22:23 +0800

    fix

--- sms.sh
+++ sms.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Load NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Load nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # Load nvm bash completion
+
+
+
 # Run the Node.js script
 node /home/markrusselbaral/smartID-backend-config/smartID-backend-nodejs/port.js
 node /home/markrusselbaral/smartID-backend-config/smartID-backend-nodejs/server.js
