d91225847f2e8e6fbf64d359954bf3bd82d38669
markrusselbaral <nullcracker04@gmail.com>
2025-02-08 10:39:02 +0800

    update

--- sms.sh
+++ sms.sh
@@ -9,6 +9,5 @@ export NVM_DIR="$HOME/.nvm"
 nvm use v20.11.1
 
 # Run the Node.js scripts in the background
-node /home/markrusselbaral/smartID-backend-config/smartID-backend-nodejs/port.js &
-node /home/markrusselbaral/smartID-backend-config/smartID-backend-nodejs/server.js &
+node /home/markrusselbaral/smartID-backend-config/smartID-backend-nodejs/port.js
     
